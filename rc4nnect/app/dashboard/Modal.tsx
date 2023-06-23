f1286d941600ebd5b5123277104911e357853a22
Jovan131 <122272142+Jovan131@users.noreply.github.com>
2023-06-23 16:11:58 +0800

    Updated modal to show information specific to each slot. Next part will be to make the 'Poll' and 'Subscribe' buttons go live

--- rc4nnect/app/dashboard/Modal.tsx
+++ rc4nnect/app/dashboard/Modal.tsx
@@ -20,7 +20,17 @@ const Modal: React.FC<AppProps> = ({slotInfo, isVisible, onClose}) => {
   const handleClose = (e: any) => {
     if (e.target.id === 'wrapper' || e.target.id === 'inner-wrapper') onClose();
   }
+
+  function addHours(date: Date, hours: number) {
+    let dummyDate = new Date(date);   // create a new copy of the date object to prevent side effects
+    dummyDate.setHours(dummyDate.getHours() + hours);
   
+    return dummyDate.toLocaleTimeString('en-SG', {
+      hour12: false,
+      hour: "2-digit", 
+      minute: "2-digit",
+    });
+  }
 
   return (
     <div className='fixed inset-0 bg-black bg-opacity-20 backdrop-blur-sm flex justify-center items-center z-10'
@@ -29,19 +39,24 @@ const Modal: React.FC<AppProps> = ({slotInfo, isVisible, onClose}) => {
         <button className='text-white text-xl place-self-end' onClick={() => onClose()}>X</button>
         <div className='bg-slate-400 p-7 rounded text-black text-xl'>
           <p className='font-normal py-3 mb-2'>
-            <span className='font-semibold text-5xl'>Squash</span>
+            <span className='font-semibold text-5xl'>{slotInfo.igName}</span>
           </p>
           <p className='font-normal py-3'>
-            <span className='font-semibold'>Date/Time:</span> 24th May 2023, 2000-2200H
+            <span className='font-semibold'>Date/Time: </span> 
+            {
+              `${slotInfo.startDateTime.toLocaleDateString('en-SG')}, 
+              ${slotInfo.startDateTime.toLocaleTimeString('en-SG', {
+                hour12: false,
+                hour: "2-digit", 
+                minute: "2-digit",
+              })}-${addHours(slotInfo.startDateTime, slotInfo.duration)}H`
+            }
           </p>
           <p className='font-normal py-3'>
-            <span className='font-semibold'>Venue:</span> USC
-          </p>
-          <p className='font-normal py-3'>
-            <span className='font-semibold'>Description:</span> Meet at RC4 B1 at 2045H!
+            <span className='font-semibold'>Venue:</span> {slotInfo.venue}
           </p>
           <p className='font-normal py-3 mb-5'>
-            <span className='font-semibold'>Slots availability:</span> 8/10
+            <span className='font-semibold'>Slots availability:</span> {slotInfo.residents.length + "/" + (slotInfo.capacity ?? "~")}
           </p>
           <div className='flex justify-between'>
             <button type="button" className="text-pink-700 hover:text-white border border-pink-700 hover:bg-pink-700 focus:ring-4 focus:outline-none focus:ring-pink-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center mr-2 mb-2 dark:border-pink-500 dark:text-pink-500 dark:hover:text-white dark:hover:bg-pink-500 dark:focus:ring-pink-800">SUBSCRIBE</button>
