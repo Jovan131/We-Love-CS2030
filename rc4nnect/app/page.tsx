905ebddec3aa76695d3673adc9184828411d6152
Jovan131 <122272142+Jovan131@users.noreply.github.com>
2023-06-25 01:41:40 +0800

    Made /login the default route again, instead of making '/' a landing page

--- rc4nnect/app/page.tsx
+++ rc4nnect/app/page.tsx
@@ -1,16 +1,34 @@
-import { getServerSession } from "next-auth";
-import { authOptions } from "./api/auth/[...nextauth]/route";
-import { LoginButton, LogoutButton } from "./auth";
+"use client"
 
-export default async function Home() {
-  const session = await getServerSession(authOptions)
+import { useEffect } from 'react';
+import { useRouter } from 'next/navigation';
 
-  return (
-    <main>
-      <LoginButton />
-      <LogoutButton />
-      <h2>Server session</h2>
-      <pre>{JSON.stringify(session)}</pre>
-    </main>
-  )
-}
+const Home: React.FC = () => {
+  const router = useRouter();
+
+  useEffect(() => {
+    router.push('/login');
+  }, []);
+
+  return null; // You can return a loading spinner or a message if needed
+};
+
+export default Home;
+
+
+// import { getServerSession } from "next-auth";
+// import { authOptions } from "./api/auth/[...nextauth]/route";
+// import { LoginButton, LogoutButton } from "./auth";
+
+// export default async function Home() {
+//   const session = await getServerSession(authOptions)
+
+//   return (
+//     <main>
+//       <LoginButton />
+//       <LogoutButton />
+//       <h2>Server session</h2>
+//       <pre>{JSON.stringify(session)}</pre>
+//     </main>
+//   )
+// }
