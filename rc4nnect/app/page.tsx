555b4faf4acf940ec31b1f7ad0ab29979559d708
haqanh <quanganhha23@gmail.com>
2023-06-25 20:49:23 +0700

    Update page.tsx

--- rc4nnect/app/page.tsx
+++ rc4nnect/app/page.tsx
@@ -3,7 +3,6 @@
 import { useEffect } from 'react';
 import { useRouter } from 'next/navigation';
 import { useSession } from 'next-auth/react';
-import Layout from '@/components/Layout';
 
 const Home: React.FC = () => {
   const router = useRouter();
@@ -19,11 +18,11 @@ const Home: React.FC = () => {
   })
 
   return (
-    <Layout>
-      <div className="flex items-center justify-center h-screen">
-        <h1 className="text-2xl">Welcome to rc4nnect...</h1>
+
+      <div className="flex items-center justify-center h-screen bg-slate-900">
+        <h1 className="text-2xl text-white">Welcome to rc4nnect...</h1>
       </div>
-    </Layout>
+
   ); // You can return a loading spinner or a message if needed
 };
 
