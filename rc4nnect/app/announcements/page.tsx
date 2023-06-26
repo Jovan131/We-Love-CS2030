1b71054713d616d8d9bdf08c2edad6a402281cda
haqanh <quanganhha23@gmail.com>
2023-06-26 12:59:22 +0700

    updated announce and settings page

---
+++ rc4nnect/app/announcements/page.tsx
@@ -0,0 +1,10 @@
+import Layout from '@/components/Layout'
+import React from 'react'
+
+export default function announcements() {
+    return (
+        <Layout routeIndex={2}>
+          <div> Coming soon! </div>
+        </Layout>
+  )
+}
---
+++ rc4nnect/app/settings/page.tsx
@@ -0,0 +1,10 @@
+import Layout from '@/components/Layout'
+import React from 'react'
+
+export default function settings() {
+    return (
+        <Layout routeIndex={3}>
+          <div> Coming soon! </div>
+        </Layout>
+  )
+}
