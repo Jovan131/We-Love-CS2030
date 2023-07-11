5f8b0bfa592f5ff40a76dc00396d67f09b20e832
Jovan131 <122272142+Jovan131@users.noreply.github.com>
2023-07-11 23:46:43 +0800

    Tabs in dashboard are working

--- rc4nnect/app/dashboard/Tabs.tsx
+++ rc4nnect/app/dashboard/Tabs.tsx
@@ -1,17 +1,23 @@
+'use client'
+
+import Link from 'next/link';
+import { usePathname } from 'next/navigation';
 import React from 'react';
 
 export default function Tabs() {
+  const pathName = usePathname()
+
   return (
     <div className='ml-3'>
       <ul className="flex flex-wrap text-sm font-medium text-center text-gray-500 border-b border-gray-200 dark:border-gray-700 dark:text-gray-400">
         <li className="mr-2">
-          <a href="#" aria-current="page" className="inline-block p-4 text-blue-600 bg-gray-100 rounded-t-lg active dark:bg-gray-800 dark:text-blue-500">Weekly Schedule</a>
+          <Link href="/dashboard/schedule"><div className={`cursor-pointer inline-block p-4 rounded-t-lg ${pathName==='/dashboard/schedule' ? 'text-blue-600 bg-gray-100' : 'hover:text-gray-600 hover:bg-gray-50'} dark:bg-gray-800 dark:text-blue-500`}>Weekly Schedule</div></Link>
         </li>
         <li className="mr-2">
-          <a href="#" className="inline-block p-4 rounded-t-lg hover:text-gray-600 hover:bg-gray-50 dark:hover:bg-gray-800 dark:hover:text-gray-300">Manage Subscribed IGs</a>
+          <div className="cursor-pointer inline-block p-4 rounded-t-lg dark:hover:bg-gray-800 dark:hover:text-gray-300">Manage Subscribed IGs</div>
         </li>
         <li className="mr-2">
-          <a href="#" className="inline-block p-4 rounded-t-lg hover:text-gray-600 hover:bg-gray-50 dark:hover:bg-gray-800 dark:hover:text-gray-300">Profile</a>
+          <Link href="/dashboard/profile"><div className={`cursor-pointer inline-block p-4 rounded-t-lg ${pathName==='/dashboard/profile' ? 'text-blue-600 bg-gray-100' : 'hover:text-gray-600 hover:bg-gray-50'} dark:hover:bg-gray-800 dark:hover:text-gray-300`}>Profile</div></Link>
         </li>
       </ul>
     </div>
