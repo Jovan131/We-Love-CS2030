0333c4ebf76dcc411134108fa3551e6b38116852
haqanh <quanganhha23@gmail.com>
2023-07-23 20:20:16 +0700

    Dark Mode

--- rc4nnect/app/layout.tsx
+++ rc4nnect/app/layout.tsx
@@ -3,17 +3,20 @@ import './globals.css'
 import Provider from './context/AuthContext'
 import ToasterContext from '@/app/context/ToasterContext'
 import { Montserrat } from 'next/font/google'
+import { Providers } from './providers'
 
 const montserrat = Montserrat({ subsets: ['latin'] })
 //const inter = Inter({ subsets: ['latin'] })
 
 export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
-    <html lang="en">
+    <html lang="en" suppressHydrationWarning>
       <body className={montserrat.className}>
         <Provider>
+          <Providers>
           <ToasterContext />
           {children}
+          </Providers>
         </Provider>
       </body>
     </html>
---
+++ rc4nnect/components/DarkMode.tsx
@@ -0,0 +1,34 @@
+'use client'
+
+import { useState, useEffect } from 'react'
+import { useTheme } from 'next-themes'
+
+
+const ThemeSwitch = () => {
+  const [mounted, setMounted] = useState(false)
+  const { theme, setTheme } = useTheme()
+
+  // useEffect only runs on the client, so now we can safely show the UI
+  useEffect(() => {
+    setMounted(true)
+  }, [])
+
+  if (!mounted) {
+    return null
+  }
+
+  if (theme === "dark") {
+    return (
+    <div className="mt-5">
+        <button type="button" className ="text-gray-900 bg-white border border-gray-300 focus:outline-none hover:bg-gray-100 font-medium rounded-full text-sm px-5 py-2.5 mr-2 mb-2" onClick={() => setTheme('light')}>Light Mode</button>
+    </div>)
+  } else {
+    return (
+    <div className="mt-5">
+        <button type="button" className ="text-white bg-gray-800 hover:bg-gray-900 font-medium rounded-full text-sm px-5 py-2.5 mr-2 mb-2" onClick={() => setTheme('dark')}>Dark Mode</button>
+    </div>)
+  }
+  
+}
+
+export default ThemeSwitch
