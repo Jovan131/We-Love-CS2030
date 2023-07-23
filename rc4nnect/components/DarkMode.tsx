13d41dee61457a8155ee68477e2cc659d65b7d77
haqanh <quanganhha23@gmail.com>
2023-07-23 20:23:14 +0700

    update current theme indicator

--- rc4nnect/components/DarkMode.tsx
+++ rc4nnect/components/DarkMode.tsx
@@ -19,13 +19,15 @@ const ThemeSwitch = () => {
 
   if (theme === "dark") {
     return (
-    <div className="mt-5">
-        <button type="button" className ="text-gray-900 bg-white border border-gray-300 focus:outline-none hover:bg-gray-100 font-medium rounded-full text-sm px-5 py-2.5 mr-2 mb-2" onClick={() => setTheme('light')}>Light Mode</button>
+    <div className="mt-5 mb-5">
+        <h1 className = "text-white font-bold text-lg"> Current theme: Dark Mode</h1>
+        <button type="button" className ="text-gray-900 bg-white border border-gray-300 focus:outline-none hover:bg-gray-100 font-medium rounded-full text-sm px-5 py-2.5 mr-2 mb-2 mt-2" onClick={() => setTheme('light')}>Light Mode</button>
     </div>)
   } else {
     return (
     <div className="mt-5">
-        <button type="button" className ="text-white bg-gray-800 hover:bg-gray-900 font-medium rounded-full text-sm px-5 py-2.5 mr-2 mb-2" onClick={() => setTheme('dark')}>Dark Mode</button>
+        <h1 className = "text-black font-bold text-lg"> Current theme: Light Mode</h1>
+        <button type="button" className ="text-white bg-gray-800 hover:bg-gray-900 font-medium rounded-full text-sm px-5 py-2.5 mr-2 mb-2 mt-2" onClick={() => setTheme('dark')}>Dark Mode</button>
     </div>)
   }
   
