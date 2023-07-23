a0b27877113ba999018c428a99d039a16d099b62
Jovan131 <122272142+Jovan131@users.noreply.github.com>
2023-07-24 01:07:44 +0800

    Added a border to allow calendar to not block other stuff

--- rc4nnect/app/manage-slots/EditButton.tsx
+++ rc4nnect/app/manage-slots/EditButton.tsx
@@ -75,7 +75,7 @@ function EditButton({ slot }: AppProps) {
               <input type="text" id="ig_name" className="cursor-not-allowed bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" 
               defaultValue={slot.igName} disabled />
             </div>
-            <div className='mb-3'>
+            <div className='mb-60'>
               <div className='flex gap-10'>
                 <div className="grow block text-sm font-medium text-gray-900 dark:text-white">
                   <p className='mb-2'>Start date/time:</p>
