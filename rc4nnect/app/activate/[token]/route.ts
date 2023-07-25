ba036b22c808b7f71f6ddd0844544b8b1b208eec
haqanh <quanganhha23@gmail.com>
2023-07-25 17:45:06 +0800

    update register

---
+++ rc4nnect/app/activate/[token]/route.ts
@@ -0,0 +1,60 @@
+import { prisma } from '@/app/db'
+import { redirect } from 'next/navigation'
+import { NextRequest } from 'next/server'
+
+export async function GET(
+  _request: NextRequest,
+  {
+    params,
+  }: {
+    params: { token: string }
+  }
+) {
+  const { token } = params
+  
+  const user = await prisma.resident.findFirst({
+    where: {
+      Token: {
+        some: {
+          AND: [
+            {
+              activatedAt: null,
+            },
+            {
+              createdAt: {
+                gt: new Date(Date.now() - 24 * 60 * 60 * 1000), // 24 hours ago
+              },
+            },
+            {
+              token,
+            },
+          ],
+        },
+      },
+    },
+  })
+
+  if (!user) {
+    throw new Error('Token is invalid or expired')
+  }
+
+  await prisma.resident.update({
+    where: {
+      id: user.id,
+    },
+    data: {
+      active: true,
+    },
+  })
+
+  await prisma.token.update({
+    where: {
+      token,
+    },
+    data: {
+      activatedAt: new Date(),
+    },
+  })
+
+  redirect('/login')
+}
